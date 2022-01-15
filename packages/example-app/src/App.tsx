40a613e11f895e61bf69861f33d038369412346b
Andre <mail@andrejilderda.nl>
2022-01-15 17:02:58 +0100

    feat: Add example app

---
+++ packages/example-app/src/App.tsx
@@ -0,0 +1,12 @@
+// import './App.css';
+import { Button } from './../../button/src/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
